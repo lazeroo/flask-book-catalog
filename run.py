b925ac49094c1a67a90e46416d167c0d9acc99d9
Robert Lasirona <rlasirona@nvidia.com>
2018-06-30 22:28:05 -0700

    bugfix: add try around create_user in run.py

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ from app.auth.models import User
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry',
-                         email='harry@potter.com',
-                         password='secret'
-                         )
-flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry',
+                             email='harry@potter.com',
+                             password='secret'
+                             )
+    except exc.IntegrityError:
+        flask_app.run()
 
